5fe50ba30b8d21a173fdd2842e7ff2bb1ac5814f
JaehonxKIM <wpmpqp1@naver.com>
2022-01-13 17:43:51 +0900

    SQL 학습 3일차 연습 및 시험

---
+++ DAY3/제목 없음.sql
@@ -0,0 +1,22 @@
+SELECT * FROM membertbl;
+
+SELECT  email, mobile, names, addr, levels 
+FROM membertbl ORDER BY names DESC;
+SELECT LOWER (EMAIL) email  FROM membertbl;
+
+ SELECT * FROM bookstbl;
+ 
+ SELECT names 
+               , author
+               ,releasedate
+               ,price
+ FROM bookstbl ORDER BY price DESC;    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
